62bd8d3459722d378c277263cc8cdcd9d5a72bf2
xcaden <xcaden2020@gmail.com>
2019-07-11 21:42:18 +0800

    handle static res

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "os"
     "net/http"
     "github.com/gorilla/mux"
     "github.com/xcaden/xblog/handlers"
@@ -10,6 +11,11 @@ func main() {
     r := mux.NewRouter()
     r.HandleFunc("/", handlers.HomeHandler)
     r.HandleFunc("/ajax/{name}", handlers.AjaxHandler)
+
+    CurrentDir,_ := os.Getwd()
+    StaticDir := CurrentDir + "/static/"
+    r.PathPrefix("/static/").Handler(
+    http.StripPrefix("/static/", http.FileServer(http.Dir(StaticDir))))
 
     http.Handle("/", r)
     http.ListenAndServe(":8080", nil)
