affbfe7d4f6a2d2d8e861da215f13c69bcd8b00b
xcaden <xcaden2020@gmail.com>
2019-07-11 21:27:58 +0800

    调整

--- handlers/home_handler.go
+++ handlers/home_handler.go
@@ -1,9 +1,21 @@
 package handlers
 
 import (
+    "os"
+    "log"
+    "html/template"
     "net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Home"))
+    CurrentDir,_ := os.Getwd()
+    HtmlDir := CurrentDir + "/static/template/home.html"
+
+    t, err := template.ParseFiles(HtmlDir)
+    if err != nil {
+        log.Fatal("template.ParseFiles : ", err)
+        return
+    }
+
+    t.Execute(w, nil)
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "os"
     "net/http"
     "github.com/gorilla/mux"
     "github.com/xcaden/xblog/handlers"
@@ -11,11 +10,6 @@ func main() {
     r := mux.NewRouter()
     r.HandleFunc("/", handlers.HomeHandler)
     r.HandleFunc("/ajax/{name}", handlers.AjaxHandler)
-
-    CurrentDir,_ := os.Getwd()
-    StaticDir := CurrentDir + "/static/"
-    r.PathPrefix("/static/").Handler(
-        http.StripPrefix("/static/", http.FileServer(http.Dir(StaticDir))))
 
     http.Handle("/", r)
     http.ListenAndServe(":8080", nil)
